122df4ea1629b00cc151cada56ce3a356720c5ac
bozzhik <bozzhik@ya.ru>
2023-10-27 20:05:26 +0300

    upgrade `seeds.rb` for creating `requests`

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-
 def seed
   reset_db
+  users = create_users(5)
   create_admin
-  create_users(5)
+  create_requests(users)
 end
 
 def reset_db
@@ -11,20 +11,41 @@ def reset_db
   Rake::Task['db:migrate'].invoke
 end
 
-def create_admin
-  user = User.create!(email: "admin@bozzhik.md", password: 'bozzhik', is_admin: true)
-  puts "Admin with #{user.email} created with id #{user.id}"
-end
-
 def create_users(num_users)
-  (1...num_users).map do |i|
+  users = []
+  (1..num_users).each do |i|
     user_data = {
       email: "user#{i}@bozzhik.md",
       password: 'bozzhik',
     }
 
     user = User.create!(user_data)
+    users << user
     puts "User with #{user.email} created with id #{user.id}"
+  end
+  users
+end
+
+def create_admin
+  user = User.create!(email: "admin@bozzhik.md", password: 'bozzhik', is_admin: true)
+  puts "Admin with #{user.email} created with id #{user.id}"
+end
+
+def create_requests(users)
+  users.each do |user|
+    next if user.is_admin?
+    
+    (1..2).each do |i|
+      request_data = {
+        title: "Custom Request #{i} for #{user.email}",
+        description: "Custom Description for Request #{i}",
+        direction: "Custom Direction for Request #{i}",
+        user: user
+      }
+
+      request = Request.create!(request_data)
+      puts "Request #{request.id} created for user #{user.email} || user id #{user.id}"
+    end
   end
 end
 
